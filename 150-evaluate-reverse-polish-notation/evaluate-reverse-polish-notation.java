54b28b4cce1bb83558f77613e55ee34a60fa9469
Midhuna Anandh <midhuna.anandh@gmail.com>
2026-09-04 14:44:04 +0530

    Time: 3 ms (99.22%) | Memory: 44.9 MB (95.16%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,6 +1,6 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        String[] stack = new String[tokens.length];
+        int[] stack = new int[tokens.length];
         int top = 0;
 
         for(int i = 0; i < tokens.length; i++){
@@ -8,34 +8,34 @@ class Solution {
             int num1, num2, result;
             switch(curr){
                 case "+":
-                    num2 = Integer.parseInt(stack[--top]);
-                    num1 = Integer.parseInt(stack[--top]);
+                    num2 = stack[--top];
+                    num1 = stack[--top];
                     result = num1 + num2;
-                    stack[top++] = String.valueOf(result);
+                    stack[top++] = result;
                     break;
                 case "-":
-                    num2 = Integer.parseInt(stack[--top]);
-                    num1 = Integer.parseInt(stack[--top]);
+                    num2 = stack[--top];
+                    num1 = stack[--top];
                     result = num1 - num2;
-                    stack[top++] = String.valueOf(result);
+                    stack[top++] = result;
                     break;
                 case "*":
-                    num2 = Integer.parseInt(stack[--top]);
-                    num1 = Integer.parseInt(stack[--top]);
+                    num2 = stack[--top];
+                    num1 = stack[--top];
                     result = num1 * num2;
-                    stack[top++] = String.valueOf(result);
+                    stack[top++] = result;
                     break;
                 case "/":
-                    num2 = Integer.parseInt(stack[--top]);
-                    num1 = Integer.parseInt(stack[--top]);
+                    num2 = stack[--top];
+                    num1 = stack[--top];
                     result = num1 / num2;
-                    stack[top++] = String.valueOf(result);
+                    stack[top++] = result;
                     break;
                 default:
-                    stack[top++] = curr;
+                    stack[top++] = Integer.parseInt(curr);
             }
         }
         
-    return Integer.parseInt(stack[--top]);
+    return stack[--top];
     }
 }
